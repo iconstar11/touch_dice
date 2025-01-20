6e9694fcb0eafe549143277dd0cfd3f8d3b6c012
Titan antony <antonytitan96@gmail.com>
2025-01-20 13:51:58 +0300

    Chore: add the color codes to the homepage

--- lib/pages/homepage.dart
+++ lib/pages/homepage.dart
@@ -2,126 +2,132 @@ import 'package:flutter/material.dart';
 import 'package:touch_dice/utils/colors.dart';
 
 class HomePage extends StatelessWidget {
-  const HomePage({Key? key}) : super(key: key);
+  const HomePage({super.key});
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: kBackgroundColor,
-      appBar: AppBar(
-        title: Text("Touch Dice",
-            style: Theme.of(context).textTheme.headlineLarge),
-        backgroundColor: kCardBackgroundColor,
-        centerTitle: true,
-        actions: [
-          IconButton(
-            onPressed: () {
-              // Add settings functionality here
-            },
-            icon: const Icon(Icons.settings),
-          ),
-        ],
-      ),
-      body: Padding(
-        padding: const EdgeInsets.all(16.0),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-          children: [
-            // Main Game Mode Options
-            Column(
-              children: [
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                  children: [
-                    // Winners Button
-                    _buildGameModeCard(
-                      context,
-                      icon: Icons.emoji_events,
-                      label: "Winners",
-                      onPressed: () {
-                        // Navigate to Winners page
-                        print("Winners clicked");
-                      },
-                    ),
-                    // Groups Button
-                    _buildGameModeCard(
-                      context,
-                      icon: Icons.group,
-                      label: "Groups",
-                      onPressed: () {
-                        // Navigate to Groups page
-                        print("Groups clicked");
-                      },
-                    ),
-                  ],
-                ),
-                const SizedBox(height: 20),
-                // Numbering Button
-                _buildGameModeCard(
-                  context,
-                  icon: Icons.format_list_numbered,
-                  label: "Numbering",
-                  onPressed: () {
-                    // Navigate to Numbering page
-                    print("Numbering clicked");
-                  },
-                ),
-              ],
-            ),
-
-            // Let's Play Button
-            ElevatedButton(
-              style: ElevatedButton.styleFrom(
-                backgroundColor: Colors.green, // Button color
-                padding:
-                    const EdgeInsets.symmetric(horizontal: 40, vertical: 15),
-                shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(30),
-                ),
-              ),
+        backgroundColor: kBackgroundColor,
+        appBar: AppBar(
+          title: Text("Touch Dice",
+              style: Theme.of(context).textTheme.headlineLarge),
+          backgroundColor: kCardBackgroundColor,
+          centerTitle: true,
+          actions: [
+            IconButton(
               onPressed: () {
-                // Start the game
-                print("Let's Play clicked");
+                // Add settings functionality here
               },
-              child: const Text(
-                "Let's Play!",
-                style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
-              ),
+              icon: const Icon(Icons.settings),
             ),
           ],
         ),
-      ),
-    );
+        body: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              // Main Game Mode Options
+              Column(
+                children: [
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    children: [
+                      // Winners Button
+                      _buildGameModeCard(
+                        context,
+                        icon: Icons.emoji_events,
+                        label: "Winners",
+                        onPressed: () {
+                          // Navigate to Winners page
+                          print("Winners clicked");
+                        },
+                        isHighlighted: true, // Highlight Winners by default
+                      ),
+                      // Groups Button
+                      _buildGameModeCard(
+                        context,
+                        icon: Icons.group,
+                        label: "Groups",
+                        onPressed: () {
+                          // Navigate to Groups page
+                          print("Groups clicked");
+                        },
+                      ),
+                    ],
+                  ),
+                  const SizedBox(height: 20),
+                  // Numbering Button
+                  _buildGameModeCard(
+                    context,
+                    icon: Icons.format_list_numbered,
+                    label: "Numbering",
+                    onPressed: () {
+                      // Navigate to Numbering page
+                      print("Numbering clicked");
+                    },
+                  ),
+                ],
+              ),
+
+              // Let's Play Button
+              ElevatedButton(
+                style: ElevatedButton.styleFrom(
+                  backgroundColor: kAccentColorGreen, // Button color
+                  padding:
+                      const EdgeInsets.symmetric(horizontal: 40, vertical: 15),
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(30),
+                  ),
+                ),
+                onPressed: () {
+                  // Start the game
+                  print("Let's Play clicked");
+                },
+                child: const Text(
+                  "Let's Play!",
+                  style: TextStyle(
+                    color: kPrimaryTextColor, // Button text color
+                    fontSize: 18,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ));
   }
 
   // Helper method to create game mode cards
   Widget _buildGameModeCard(BuildContext context,
       {required IconData icon,
       required String label,
-      required VoidCallback onPressed}) {
+      required VoidCallback onPressed,
+      bool isHighlighted = false}) {
     return GestureDetector(
       onTap: onPressed,
       child: Container(
         width: 150,
         height: 150,
         decoration: BoxDecoration(
-          color: Colors.grey[800],
+          color: kCardBackgroundColor, // Card background color
           borderRadius: BorderRadius.circular(20),
           border: Border.all(
-            color: Colors.green,
-            width: label == "Winners"
-                ? 2.0
-                : 0.0, // Highlight 'Winners' by default
+            color: isHighlighted ? kAccentColorGreen : Colors.transparent,
+            width: 2.0,
           ),
         ),
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            Icon(icon, size: 50, color: Colors.white),
+            Icon(icon, size: 50, color: kPrimaryTextColor), // Icon color
             const SizedBox(height: 10),
             Text(
               label,
-              style: const TextStyle(color: Colors.white, fontSize: 18),
+              style: const TextStyle(
+                color: kPrimaryTextColor, // Text color
+                fontSize: 18,
+              ),
             ),
           ],
         ),
