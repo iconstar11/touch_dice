08aee196693062a2a2d4382c167e00286dca8e04
Titan antony <antonytitan96@gmail.com>
2025-01-20 14:33:28 +0300

    Chore: add a mockup of the winner screen

--- lib/routes.dart
+++ lib/routes.dart
@@ -1,8 +1,10 @@
 import 'package:get/get.dart';
 import 'package:touch_dice/pages/homepage.dart';
+import 'package:touch_dice/pages/winnersScreen.dart';
 
 class Routes {
   static var routes = [
     GetPage(name: "/First-Page", page: () => const HomePage()),
+    GetPage(name: "/Winner-Page", page: () => const WinnersScreen()),
   ];
 }
