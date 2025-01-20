22c5203aed9dee14c6163f5cdb574d1389ccca3c
Titan antony <antonytitan96@gmail.com>
2025-01-20 13:37:07 +0300

    Chore: Add the color code for the app

---
+++ lib/utils/colors.dart
@@ -0,0 +1,13 @@
+// Primary Colors
+import 'dart:ui';
+
+const Color kBackgroundColor = Color(0xFF1F1F1F); // Dark Gray
+const Color kCardBackgroundColor = Color(0xFF2A2A2A); // Lighter Gray
+const Color kAccentColorGold = Color(0xFFFFD700); // Gold
+const Color kAccentColorGreen = Color(0xFF00C853); // Bright Green
+const Color kAccentColorBlue = Color(0xFF29B6F6); // Light Blue
+const Color kAccentColorPink = Color(0xFFFF4081); // Hot Pink
+
+// Text Colors
+const Color kPrimaryTextColor = Color(0xFFFFFFFF); // White
+const Color kSecondaryTextColor = Color(0xFFBDBDBD); // Light Gray
